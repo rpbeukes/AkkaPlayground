9638c54b20338f6a2e95e1b4eb12f19a1bf720fd
Ruan Beukes <ruan.coder@gmail.com>
2015-06-30 20:36:09 +0800

    Minor change - move messages into partial class.

---
+++ src/Unit 2/Lesson2.2 Using ReceiveActor for Smarter Message Handling/Actors/ChartingActor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    public partial class ChartingActor : ReceiveActor
+    {
+        private readonly Chart _chart;
+        private Dictionary<string, Series> _seriesIndex;
+
+        public ChartingActor(Chart chart)
+            : this(chart, new Dictionary<string, Series>())
+        {
+        }
+
+        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
+        {
+            _chart = chart;
+            _seriesIndex = seriesIndex;
+
+            Receive<InitializeChart>(ic => HandleInitialize(ic));
+            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+        }
+
+        #region Individual Message Type Handlers
+
+        private void HandleInitialize(InitializeChart ic)
+        {
+            if (ic.InitialSeries != null)
+            {
+                //swap the two series out
+                _seriesIndex = ic.InitialSeries;
+            }
+
+            //delete any existing series
+            _chart.Series.Clear();
+
+            //attempt to render the initial chart
+            if (_seriesIndex.Any())
+            {
+                foreach (var series in _seriesIndex)
+                {
+                    //force both the chart and the internal index to use the same names
+                    series.Value.Name = series.Key;
+                    _chart.Series.Add(series.Value);
+                }
+            }
+        }
+
+        private void HandleAddSeries(AddSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name))
+            {
+                _seriesIndex.Add(series.Series.Name, series.Series);
+                _chart.Series.Add(series.Series);
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/Unit 2/Lesson2.2 Using ReceiveActor for Smarter Message Handling/Main.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ChartApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textCommand1 = new DevExpress.CodeRush.Core.TextCommand(this.components);
+            this.buttonAddSeries = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textCommand1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sysChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.sysChart.ChartAreas.Add(chartArea2);
+            this.sysChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.sysChart.Legends.Add(legend2);
+            this.sysChart.Location = new System.Drawing.Point(0, 0);
+            this.sysChart.Name = "sysChart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.sysChart.Series.Add(series2);
+            this.sysChart.Size = new System.Drawing.Size(684, 446);
+            this.sysChart.TabIndex = 0;
+            this.sysChart.Text = "sysChart";
+            // 
+            // textCommand1
+            // 
+            this.textCommand1.CommandName = null;
+            this.textCommand1.Description = null;
+            // 
+            // buttonAddSeries
+            // 
+            this.buttonAddSeries.Location = new System.Drawing.Point(567, 381);
+            this.buttonAddSeries.Name = "buttonAddSeries";
+            this.buttonAddSeries.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddSeries.TabIndex = 1;
+            this.buttonAddSeries.Text = "&Add Series";
+            this.buttonAddSeries.UseVisualStyleBackColor = true;
+            this.buttonAddSeries.Click += new System.EventHandler(this.buttonAddSeries_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 446);
+            this.Controls.Add(this.buttonAddSeries);
+            this.Controls.Add(this.sysChart);
+            this.Name = "Main";
+            this.Text = "System Metrics";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sysChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textCommand1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
+        private DevExpress.CodeRush.Core.TextCommand textCommand1;
+        private System.Windows.Forms.Button buttonAddSeries;
+    }
+}
+

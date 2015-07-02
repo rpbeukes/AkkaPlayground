c866811b1883db45d61613ad74cecd41387916dd
Ruan Beukes <ruan.coder@gmail.com>
2015-07-02 21:06:32 +0800

    add files

---
+++ src/Unit 2/Lesson2.4 Switching Actor Behavior at Run-time with BecomeStacked and UnbecomeStacked/Actors/AddSeries.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ChartApp.Actors
+{
+    public partial class ChartingActor : ReceiveActor
+    {
+        public class AddSeries
+        {
+            public AddSeries(Series series)
+            {
+                Series = series;
+            }
+
+            public Series Series { get; set; }
+        }
+    }
+}
